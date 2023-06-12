1949bb3b6b832badf7b8e9bf1b9b7aeee671855f
DawoodKamar <114121464+DawoodKamar@users.noreply.github.com>
2023-06-12 07:55:41 -0400

    Adjusted the header design to a more thin modern look

--- components/header.js
+++ components/header.js
@@ -28,49 +28,52 @@ const toggleMenu = () => {
 
     <header className={styles.header}>
         <div className={styles.banner}>
-            <h1 className={styles.logo}>DK Services</h1>
-            <div className={`${styles.hamburgerMenu} ${showMenu ? styles.open : ''}`} 
-            onClick={toggleMenu}> 
-                {/* If showMenu is true,  styles.hamburgerMenu styles.open , otherwise styles.hamburgerMenu */}
             
-                {/* `${}` template literal: allow you to embed expressions inside your strings, 
-                anything inside `${}` will be evaluated as a JavaScript expression. */}
+                <Link href="/" className={styles.logo}> DK Services</Link>
+                <div className={`${styles.hamburgerMenu} ${showMenu ? styles.open : ''}`} 
+                onClick={toggleMenu}> 
+                    {/* If showMenu is true,  styles.hamburgerMenu styles.open , otherwise styles.hamburgerMenu */}
+                
+                    {/* `${}` template literal: allow you to embed expressions inside your strings, 
+                    anything inside `${}` will be evaluated as a JavaScript expression. */}
 
-                {/* showMenu ? styles.open : '' is a ternary operator. This is a shortcut for an 
-                if-else statement. It reads like this: "If showMenu is true, use styles.open, otherwise use an empty string. */}
+                    {/* showMenu ? styles.open : '' is a ternary operator. This is a shortcut for an 
+                    if-else statement. It reads like this: "If showMenu is true, use styles.open, otherwise use an empty string. */}
 
-                <div className={styles.line}></div>
-                <div className={styles.line}></div>
-                <div className={styles.line}></div>
-            </div>
+                    <div className={styles.line}></div>
+                    <div className={styles.line}></div>
+                    <div className={styles.line}></div>
+                </div>
+            
+            
+    
+                <nav className={styles.navbar}>
+                    <ul>
+                        <li>
+                            <Link href="/">
+                            Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/about">
+                            About Us
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/contact">
+                            Contact
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/WorkOrder">
+                            Mechanic Login
+                            </Link>
+                        </li>
+                    </ul>
+
+                </nav>
+            
         </div>
-        
-  
-        <nav className={styles.navbar}>
-            <ul>
-                <li>
-                    <Link href="/">
-                    Home
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/about">
-                    About Us
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/contact">
-                    Contact
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/WorkOrder">
-                    Mechanic Login
-                    </Link>
-                </li>
-            </ul>
-
-        </nav>
 
         <nav className={`${styles.smallnavbar} ${!showMenu ? styles.close : ''}`}>
             <ul>
@@ -102,4 +105,4 @@ const toggleMenu = () => {
      </header>
 
     );
-}//comment the hamburger line
+}//hide menu on bigger screens
