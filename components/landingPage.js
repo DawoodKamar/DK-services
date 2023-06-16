1abafcfe8f1f9b744a2b664b966bf5dd694ac45b
DawoodKamar <114121464+DawoodKamar@users.noreply.github.com>
2023-06-16 07:58:40 -0400

    starting the skeleton of the hero section

--- components/landingPage.js
+++ components/landingPage.js
@@ -1,28 +1,22 @@
 import Layout from "./layout";
+import styles from '../styles/landingPage.module.css';
 
 export default function LandingPage(){
     return(
     <Layout>
   
-      <section>
-        <h1>HERO SECTIONs</h1>
-          <h1>HERO SECTIONs</h1>
-          <h1>HERO SECTIONs</h1>
-          <h1>HERO SECTIONs</h1>
-          <h1>HERO SECTIONs</h1>
-          <h1>HERO SECTIONs</h1>
-          <h1>HERO SECTIONs</h1>
-          <h1>HERO SECTIONs</h1>
-          <h1>HERO SECTIONs</h1>
-          <h1>HERO SECTIONs</h1>
-          <h1>HERO SECTIONs</h1>
-          <h1>HERO SECTIONs</h1>
-          <h1>HERO SECTIONs</h1>
-          <h1>HERO SECTIONs</h1>
-          <h1>HERO SECTIONs</h1>
-          <h1>HERO SECTIONs</h1>
-        {/* Hero section */}
-      </section>
+      <div className={styles.container}>
+        <div className={styles.heroSection}>
+          <div className={styles.herohead}><h1>Lorem ipsum dolor sit amet consnderit autem necessitatibus, eum consectetur?</h1></div>
+          <div className={styles.hook}><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet atque minima error optio debitis ab, assumenda molestiae deserunt alias veniam aut suscipit iste repellat labore rem esse in dolorum impedit.</p></div>
+        </div>
+        <div className={styles.img1}>test</div>  
+        <div className={styles.img2}>test</div>  
+        <div className={styles.img3}>test</div>
+
+
+
+      </div>
       
     </Layout>
   );
