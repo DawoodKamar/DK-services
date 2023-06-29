1f8f857fef2db4f4ab2bab5e969f7f03f26b80d9
DawoodKamar <114121464+DawoodKamar@users.noreply.github.com>
2023-06-29 08:11:06 -0400

    finish home page mvp

--- components/landingPage.js
+++ components/landingPage.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import dry from "../public/images/Dry-Van.png"
 import reefer from "../public/images/DuraPlate_HD.png"
 import working from "../public/images/working.jpg"
+import ContactForm from "./ContactForm";
 
 
 export default function LandingPage(){
@@ -82,8 +83,9 @@ export default function LandingPage(){
           </div>
 
 
- 
       </section>
+
+      <ContactForm/>
       
     </Layout>
   );
