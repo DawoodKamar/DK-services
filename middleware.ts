07f263384c76ae7816ea0fc95b7e4b6352f32f95
DawoodKamar <114121464+DawoodKamar@users.noreply.github.com>
2023-08-07 12:59:16 -0400

    added back the header changes and now functional workorder fetch

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-
 export default authMiddleware({
-    publicRoutes: ["/", "/About", "/Contact",]
-  });
+    publicRoutes: ["/", "/api/(.*)", "/contact", "/about"]
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
